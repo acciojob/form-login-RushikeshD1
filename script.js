312c64eb3bde66ff5f185d786b07800cfec29775
RushikeshD1 <141423104+RushikeshD1@users.noreply.github.com>
2026-03-14 22:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function getFormvalue(e) {
+function getFormvalue() {
     //Write your code here
-	e.preventDefault();
+	// e.preventDefault();
 	
 	const firstName = document.querySelector('input[name="fname"]').value;
 	const lastName = document.querySelector('input[name="lname"]').value;
 
+	if (firstName === '' || lastName === ''){
+		return;
+	}
 	
 	alert(`${firstName.trim()} ${lastName.trim()}`);
 }
